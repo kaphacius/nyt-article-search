0c9d2e12f70bb5656a3b167869b6694a3fd69238
Yurii Zadoianchuk <yurii@styleshoots.com>
2021-01-24 11:03:49 +0100

    Fix typo in API key reminder

--- SnoitcaPirt/SnoitcaPirt/SnoitcaPirtApp.swift
+++ SnoitcaPirt/SnoitcaPirt/SnoitcaPirtApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SnoitcaPirtApp: App {
     let network: Network = {
         let url = URL(string: "api.nytimes.com")!
-        #error("Insert your API ket here. Can be obtained at https://developer.nytimes.com/my-apps")
+        #error("Insert your API key here. Can be obtained at https://developer.nytimes.com/my-apps")
         let auth = URLQueryItem(name: "api-key", value: <#T##String#>)
         let nytNetwork = Network(host: url, scheme: .https, staticQuery: [auth])
 
